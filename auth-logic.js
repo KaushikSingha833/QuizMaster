77990e5b9775f49ad7e1358633f6cefee636a5e0
Kaushik Singha <kaushiksingha081@gmail.com>
2026-07-09 21:55:56 +0530

    Fix: Corrected firebase-config import path

--- auth-logic.js
+++ auth-logic.js
@@ -1,4 +1,4 @@
-import { auth } from '../Home/firebase-config.js';
+import { auth } from './firebase-config.js';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const validatePassword = (password) => {
